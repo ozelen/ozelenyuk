841f7459526845696237aa8a0cd2e13474f2c402
Oleksiy Zelenyuk <oleksadesign@gmail.com>
2013-02-11 20:22:20 +0200

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oleksa::Application.config.secret_token = 'c0ea18416acf0ebecc5e0e1ce487fa002c38f427ab1bee7353cc57c3a4562f6933a4c88c4acca52980ccb782ceb94d8962b8a05fbf53d01a8310ffbc3a42c221'
